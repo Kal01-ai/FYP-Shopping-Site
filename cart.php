364eac671b32d5ae1106960235f1225b1c7a14ba
Haikal Ahmad <haikalahmad130@gmail.com>
2021-10-31 23:06:10 +0800

    Revised cart.php
    Change vertical alignment image to horizontal inside while loop

--- cart.php
+++ cart.php
@@ -6,48 +6,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-    <link href="style.css" rel="stylesheet">
+    <link href="cart.css" rel="stylesheet">
     <title>Checkout</title>
 </head>
 <body>
-    <div class="container" style="color: white;">
-    <?php
+    <div class="container">
+    <!--PHP Here-->
+    <?php 
+    
+        $connect = mysqli_connect('localhost', 'root', '', 'kerepekdb');
+        $query = 'SELECT * FROM product ORDER BY id ASC';
+        $result = mysqli_query($connect, $query);
 
-    $servername="localhost";
-    $username="root";
-    $password="";
+        if($result) :
+            $i=0;
+            echo '<table><tr>';
+            if(mysqli_num_rows($result)>0) :
+                while($product = mysqli_fetch_assoc($result)) :
+                    //print_r($product);
+                    ?>
 
-    $dbase="kerepekdb";
+                    <td>
+                        <div class="product">
+                            <form method="post" action="cart.php?action=add&id=<?php echo $product['id']; ?>">
+                                <div class="row row-cols-1 row-cols-md-3 g-4"></div>
+                            </form>
+                        </div>
+                    </td>
 
-    $conn=new mysqli($servername, $username, $password, $dbase);
-
-    if ($conn->connect_error) {
-        die("Connection failed: " .$conn->connect_error);
-    } else
-        $sql="SELECT * FROM product";
-        $result=$conn->query($sql);
-
-        if($result->num_rows>0):
-            while($row=$result->fetch_assoc()): //print_r($row);
-            ?>
-            
-            <div class="row row-cols-1 row-cols-md-3 g-4">
-            <div class="col">
-                <div class="card h-100">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                </div>
-                <div class="card-footer">
-                    <small class="text-muted">Last updated 3 mins ago</small>
-                </div>
-                </div>
-            </div>
-            </div>
-
-            <?php
-            endwhile;
+                <?php
+                    $i++;
+                    if($i==5) {
+                        echo '</tr><tr>';
+                    }
+                endwhile;
+                    echo '</tr></table>';
+            endif;
         endif;
     ?>
     </div>
