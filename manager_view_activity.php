cd3b6cc503ce4d88977b737a08ccc0fe7eb0f5ba
Haikal Ahmad <haikalahmad130@gmail.com>
2021-11-30 21:24:58 +0800

    More manager function

--- manager_view_activity.php
+++ manager_view_activity.php
@@ -7,7 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     <link href="#" rel="stylesheet">
-    <title>Manager Home Page</title>
+    <title>Admin Activity</title>
 </head>
 <body>
     <!--PHP to verify manager session is active or not-->
@@ -18,49 +18,46 @@
         }
     ?>
 
-    <!--Product Table-->
+    <!--admin Table-->
     <div class="container pt-5">
         <div class="d-grid gap-2 d-md-flex justify-content-md-end">
             <h2>Welcome, Manager</h2>
         </div>
         <div class="pb-5 d-grid gap-2 d-md-flex justify-content-md-end">
-            <a href="index.html" class="btn btn-secondary">Go to main website</a>
+        <a href="manager_panel.php" class="btn btn-secondary">Go back to main panel</a>
             <a href="manager.php?logout=true" class="btn btn-danger">Log Out</a>
         </div>
 
-    <a href="manager_manage_admin.php" class="btn btn-primary">Manage Admin Account</a>
-    <a href="manager_view_activity.php" class="btn btn-primary">View Admin Activity</a>
-    <a href="manager_view_customer.php" class="btn btn-primary">View Customer Contact</a>
     <div style="clear:both"></div>
         <br>
         
         <div class="table-responsive">
             <table class="table">
-                <tr><th colspan="5"><h3>Product Details</h3></th></tr>
+                <tr><th colspan="5"><h3>Admin Activity</h3></th></tr>
                 <tr>
-                  <th width="30%">Product Name</th>
-                  <th width="10%">Price</th>
-                  <th width="25%">Product Description</th>
-                  <th width="25%">Image</th>
+                  <th width="10%">Admin Email</th>
+                  <th width="20%">Description of Action</th>
+                  <th width="10%">Action Performed</th>
+                  <th width="10%">Date and Time</th>
                 </tr>
 
         <?php
         $connect = mysqli_connect('localhost', 'root', 'RoxaR1234', 'kerepekdb');
-        $query = 'SELECT * FROM product ORDER BY id ASC';
+        $query = 'SELECT * FROM activity_product ORDER BY id ASC';
         $result = mysqli_query($connect, $query);
 
         if($result) :
             if(mysqli_num_rows($result)>0) :
-                while($product = mysqli_fetch_assoc($result)) :
-                    //print_r($product);
-                    $id = $product['id'];
+                while($admin = mysqli_fetch_assoc($result)) :
+                    //print_r($admin);
+                    $id = $admin['id'];
         ?>
 
                 <tr>
-                  <td><?php echo $product['name']; ?></td>
-                  <td>RM <?php echo $product['price']; ?></td>
-                  <td><?php echo $product['product_desc']; ?></td>
-                  <td><img style="width:100%;" src="<?php echo $product['image']; ?>" alt="Product Image"></td>
+                  <td><?php echo $admin['admin_email']; ?></td>
+                  <td><?php echo $admin['admin_description']; ?></td>
+                  <td><?php echo $admin['action_performed']; ?></td>
+                  <td><?php echo $admin['date_time']; ?></td>
                 </tr>
 
             <?php
