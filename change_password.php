0179b0de94d7b80f3eef5f72f80d097d3e0f15a2
Haikal Ahmad <haikalahmad130@gmail.com>
2021-12-12 20:34:18 +0800

    Change Password

--- change_password.php
+++ change_password.php
@@ -19,23 +19,18 @@
 
     <!--Login form-->
     <main class="form-signin">
-        <form action="login-success.php" method="post">
+        <form action="request.php" method="post">
           <img class="mb-4" src="img/kerepek-r-us-logo.png" alt="Logo" width="140" height="140">
-          <h1 class="h3 mb-3 fw-normal" style="color: white;">Please sign in</h1>
+          <h1 class="h3 mb-3 fw-normal" style="color: white;">Request Password Change</h1>
       
-          <div class="form-floating">
+          <div class="form-floating pb-3">
             <input type="email" name="email" class="form-control" id="floatingInput" placeholder="name@example.com" required>
             <label for="floatingInput">Email</label>
           </div>
-          <div class="form-floating">
-            <input type="password" name="password" class="form-control" id="floatingPassword" placeholder="Password" required>
-            <label for="floatingPassword">Password</label>
-          </div>
-          <button class="w-100 btn btn-lg btn-primary" type="submit">Log in</button>
+          <button class="w-100 btn btn-lg btn-primary" type="submit">Request</button>
           <div class="container pt-3 pb-3">
-            <a class="btn btn-secondary btn-lg" href="index.php" role="button">Go home</a>
+            <a class="btn btn-secondary btn-lg" href="login.php" role="button">Go back to login</a>
           </div>
-          <a style="color: white;" href="change_password.php">Forgot password?</a>
           <p class="mt-5 mb-3 text-muted">Copyright &copy; 2021 Kerepek R Us</p>
         </form>
       </main>          
